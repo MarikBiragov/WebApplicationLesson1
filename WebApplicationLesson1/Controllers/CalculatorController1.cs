dcbac9d4cc1ff19ef32accb879ca302e9ecb9e7c
MarikBiragov <121573561+MarikBiragov@users.noreply.github.com>
2024-03-31 16:39:58 +0300

    Продолжаю делать задания со Степика

---
+++ WebApplicationLesson1/Controllers/CalculatorController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a = 0, int b = 0, string c = "+")
+        {
+            switch (c)
+            {
+                case "+":
+                    {
+                        return $"{a} + {b} = {a + b}";
+                    }
+                case "-":
+                    {
+                        return $"{a} - {b} = {a - b}";
+                    }
+                case "*":
+                    {
+                        return $"{a} * {b} = {a * b}";
+                    }
+                case "/":
+                    {
+                        return $"{a} / {b} = {a / b}";
+                    }
+
+                default:
+                    return "Ошибка операции";
+            }
+        }
+    }
+}
